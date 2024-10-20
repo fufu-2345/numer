6288b935ddf555d6a0b456b989e43dacd979ad91
fufu-2345 <mr.kittisak.ploysumrit@gmail.com>
2024-10-20 20:46:23 +0700

    V22

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ const port =5000;
 
 //  ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Abc810254795342320121448';
 
+//  https://www.youtube.com/watch?v=G-MWE7_8dnA
+
+
 const db= mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -26,5 +29,5 @@ app.get("/cramerrule", (req,res) =>{
 
 })
  
-app.listen(port,() => {console.log(`port ${port} is updated/started`)})
+app.listen(port,() => {console.log(`port ${port}(server) is updated/started`)})
 
